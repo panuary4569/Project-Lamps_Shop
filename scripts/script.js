371c8b179b5a1604c4ce6749fe8033e7681b0191
Kostya <57058871+panuary4569@users.noreply.github.com>
2020-02-11 17:37:59 +0300

    product adaptive and logic

--- scripts/script.js
+++ scripts/script.js
@@ -51,5 +51,54 @@
 			}
 		}
 		]
+	});
+
+
+	var quanity = {
+		'Summer Dress': 0,
+		'Winter Dress': 0,
+		'Spring Dress': 0,
+		'Autumn Dress': 0
+	}
+	console.log(quanity['Summer Dress']);
+	$('.minusBtn').on('click', function(){
+		var a = $(this).siblings('i').text();
+		var b = $(this);
+		if( (a === 'Winter Dress') && (quanity['Winter Dress'] > 0) ){
+			quanity['Winter Dress'] = quanity['Winter Dress'] - 1;
+			b.siblings('.quanityNum').text(quanity['Winter Dress']);
+			console.log(quanity['Winter Dress']);
+		}else if( (a === 'Spring Dress')  && (quanity['Spring Dress'] > 0) ){
+			quanity['Spring Dress'] = quanity['Spring Dress'] - 1;
+			b.siblings('.quanityNum').text(quanity['Spring Dress']); 
+		}
+		else if( (a === 'Summer Dress')  && (quanity['Summer Dress'] > 0) ){
+			quanity['Summer Dress'] = quanity['Summer Dress'] - 1;
+			b.siblings('.quanityNum').text(quanity['Summer Dress']); 
+		}
+		else if( (a === 'Autumn Dress')  && (quanity['Autumn Dress'] > 0) ){
+			quanity['Autumn Dress'] = quanity['Autumn Dress'] - 1;
+			b.siblings('.quanityNum').text(quanity['Autumn Dress']); 
+		}
+	});
+	$('.plusBtn').on('click', function(){
+		var a = $(this).siblings('i').text();
+		var b = $(this);
+		if( (a === 'Winter Dress') && (quanity['Winter Dress'] < 100)){
+			quanity['Winter Dress'] = quanity['Winter Dress'] + 1;
+			b.siblings('.quanityNum').text(quanity['Winter Dress']);
+		}else if( (a === 'Spring Dress')  && (quanity['Spring Dress'] < 100) ){
+			quanity['Spring Dress'] = quanity['Spring Dress'] + 1;
+			b.siblings('.quanityNum').text(quanity['Spring Dress']); 
+		}
+		else if( (a === 'Summer Dress')  && (quanity['Summer Dress'] < 100) ){
+			quanity['Summer Dress'] = quanity['Summer Dress'] + 1;
+			b.siblings('.quanityNum').text(quanity['Summer Dress']); 
+		}
+		else if( (a === 'Autumn Dress')  && (quanity['Autumn Dress'] < 100) ){
+			quanity['Autumn Dress'] = quanity['Autumn Dress'] + 1;
+			b.siblings('.quanityNum').text(quanity['Autumn Dress']); 
+		}
 	})
+
 })(jQuery);
