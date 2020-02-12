03915219ab15b301a591b3410ea6a5535f7668f9
Kostya <57058871+panuary4569@users.noreply.github.com>
2020-02-12 21:19:37 +0300

    Почти доделал лоигку магазина
    Работа над показом купленых товаров

--- scripts/script.js
+++ scripts/script.js
@@ -54,62 +54,70 @@
 	});
 
 
-	var quanity = {
-		'Summer Dress': 0,
-		'Summer Dress Red' : 0,
-		'Summer Dress Blue' : 0,
-		'Summer Dress Green' : 0,
-		'Winter Dress': 0,
-		'Winter Dress Red': 0,
-		'Winter Dress Blue': 0,
-		'Winter Dress Green': 0,
-		'Spring Dress': 0,
-		'Spring Dress Red': 0,
-		'Spring Dress Blue': 0,
-		'Spring Dress Green': 0,
-		'Autumn Dress': 0,
-		'Autumn Dress Red': 0,
-		'Autumn Dress Blue': 0,
-		'Autumn Dress Green': 0
+	var quanityPrice = {
+		'Summer Dress': [21, 0], //productName : [price, quanity]
+		'Summer Dress Red' : [21, 0],
+		'Summer Dress Blue' : [21, 0],
+		'Summer Dress Green' : [21, 0],
+		'Winter Dress': [23, 0],
+		'Winter Dress Red': [23, 0],
+		'Winter Dress Blue': [23, 0],
+		'Winter Dress Green': [23, 0],
+		'Spring Dress': [29, 0],
+		'Spring Dress Red': [29, 0],
+		'Spring Dress Blue': [29, 0],
+		'Spring Dress Green': [29, 0],
+		'Autumn Dress': [27, 0],
+		'Autumn Dress Red': [27, 0],
+		'Autumn Dress Blue': [27, 0],
+		'Autumn Dress Green': [27, 0]
 	}
-	console.log(quanity['Summer Dress']);
+	var productInCart = {
+		'#1' : [0],
+	}
+	// for (key in quanityPrice){
+	// 	console.log(key, quanityPrice[key][0], quanityPrice[key][1]);
+	// }
 	$('.minusBtn').on('click', function(){
-		var a = $(this).siblings('i').text();
-		var b = $(this);
-		if( (a === 'Winter Dress') && (quanity['Winter Dress'] > 0) ){
-			quanity['Winter Dress'] = quanity['Winter Dress'] - 1;
-			b.siblings('.quanityNum').text(quanity['Winter Dress']);
-			console.log(quanity['Winter Dress']);
-		}else if( (a === 'Spring Dress')  && (quanity['Spring Dress'] > 0) ){
-			quanity['Spring Dress'] = quanity['Spring Dress'] - 1;
-			b.siblings('.quanityNum').text(quanity['Spring Dress']); 
-		}
-		else if( (a === 'Summer Dress')  && (quanity['Summer Dress'] > 0) ){
-			quanity['Summer Dress'] = quanity['Summer Dress'] - 1;
-			b.siblings('.quanityNum').text(quanity['Summer Dress']); 
-		}
-		else if( (a === 'Autumn Dress')  && (quanity['Autumn Dress'] > 0) ){
-			quanity['Autumn Dress'] = quanity['Autumn Dress'] - 1;
-			b.siblings('.quanityNum').text(quanity['Autumn Dress']); 
-		}
+		var productName = $(this).siblings('i').text(), 
+			//Узнаём имя товара
+			minBtn = $(this),
+			productPrice = $(this).closest('.productItem').find('.productPrice'), 
+			//Находим блок с ценой товара
+			colorPr = $(this).closest('.colTwo').find('.productColor');
+			//Находим блок с цветом товара
+		productName = productName + colorPr.text(); 
+			// Склеиваем значения для поиска по обьекту quanityPrice
+
+			for( key in quanityPrice) {
+				// Перебираем массив (обьект)
+				if( key === productName && quanityPrice[key][1] > 0) {
+					// Как только имя товара совпало а его количество больше нуля тогда
+					quanityPrice[key][1] = quanityPrice[key][1] - 1;
+					// Уменьшаем его количество на единицу
+					minBtn.siblings('.quanityNum').text(quanityPrice[key][1]);
+					// Записываем количество в специальный блок
+					productPrice.text('$' + (quanityPrice[key][0] * quanityPrice[key][1]));
+					// Записываем в блок с ценой товара его значение умноженое на количество товара
+					// console.log(key, quanityPrice[key]);
+				}
+
+			}
 	});
 	$('.plusBtn').on('click', function(){
-		var a = $(this).siblings('i').text();
-		var b = $(this);
-		if( (a === 'Winter Dress') && (quanity['Winter Dress'] < 100)){
-			quanity['Winter Dress'] = quanity['Winter Dress'] + 1;
-			b.siblings('.quanityNum').text(quanity['Winter Dress']);
-		}else if( (a === 'Spring Dress')  && (quanity['Spring Dress'] < 100) ){
-			quanity['Spring Dress'] = quanity['Spring Dress'] + 1;
-			b.siblings('.quanityNum').text(quanity['Spring Dress']); 
-		}
-		else if( (a === 'Summer Dress')  && (quanity['Summer Dress'] < 100) ){
-			quanity['Summer Dress'] = quanity['Summer Dress'] + 1;
-			b.siblings('.quanityNum').text(quanity['Summer Dress']); 
-		}
-		else if( (a === 'Autumn Dress')  && (quanity['Autumn Dress'] < 100) ){
-			quanity['Autumn Dress'] = quanity['Autumn Dress'] + 1;
-			b.siblings('.quanityNum').text(quanity['Autumn Dress']); 
+		var productName = $(this).siblings('i').text(),
+			plsBtn = $(this),
+			productPrice = $(this).closest('.productItem').find('.productPrice'), 
+			colorPr = $(this).closest('.colTwo').find('.productColor');
+		productName = productName + colorPr.text();
+
+		for( key in quanityPrice ) {
+			if( key === productName && quanityPrice[key][1] < 100) {
+				quanityPrice[key][1] = quanityPrice[key][1] + 1;
+				plsBtn.siblings('.quanityNum').text(quanityPrice[key][1]);
+				productPrice.text('$' + (quanityPrice[key][0] * quanityPrice[key][1]));
+				// console.log(key, quanityPrice[key]);
+			}
 		}
 	});
 	
@@ -117,21 +125,62 @@
 	// COLORBUTTONS
 
 	$('.colorBtn').on('click', function(){
-		var colorB = $(this);
+		var colorB = $(this),
+			colorData = $(this).siblings('.productColor');
+
 		function backImageColorChange (color) {
 			var bbb = colorB.closest('.colTwo').find('.productData').text();
-			// colorB.closest('.colTwo').find('.productData').text(bbb + color);
 			bbb = 'url("./image/' + bbb + color + '.png")';
 			colorB.closest('.productItem').children('.colOne').css({'background-image' : bbb});
-			console.log(bbb)
+			// console.log(bbb)
 		}
 		if(colorB.hasClass('-backGreen')){
+			colorData.text(' Green');
 			backImageColorChange(' Green');
 		}else if(colorB.hasClass('-backBlue')){
+			colorData.text(' Blue');
 			backImageColorChange(' Blue')
 		}else if(colorB.hasClass('-backRed')){
+			colorData.text(' Red');
 			backImageColorChange(' Red');
 		};
 	});
+
+	//BUYBTN
+	var cartNum = $('.cartCost').text();
+
+	$('.buyBtn').on('click', function(){
+		var buyBtn = $(this),
+			buyNum = $(this).siblings('.quanity').find('.quanityNum'),
+			buyDataName = $(this).siblings('.quanity').find('.productData').text(),
+			buyDataColor = $(this).siblings('.colorChoose').find('.productColor').text(),
+			buyPrice = $(this).siblings('.productPrice').text();
+			cartNum = Number(cartNum) + Number(buyNum.text());
+		if(buyNum.text() > 0){
+			buyName = buyDataName + buyDataColor;
+
+		setTimeout(function(){
+			productInCart[buyPrice] = [buyName, buyPrice, buyNum.text()];
+			// var boughtProduct = buyName + ' ' + buyPrice + ' ' + buyNum.text();
+			$('.cartCost').addClass('-showContent');
+			$('.cartCost').text(cartNum);
+		}, 200);
+		}
+		console.log(productInCart);
+	});
+
+
+	$('.cartBtn').on('click', function(){
+		var a;
+		if( $('.cartCost').text() == 0 ){
+			alert('No product (');
+		}else{
+			for(key in productInCart){
+				a = '\n' + a + (key, productInCart[key][0], productInCart[key][1])
+			}
+			alert(a);
+			console.log(a);
+		}
+	})
 
 })(jQuery);
